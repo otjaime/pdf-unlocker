c6fe4bad16e8939ee35557273d2a319a07732818
otjaime <31783413+otjaime@users.noreply.github.com>
2025-06-02 18:15:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,5 @@ def unlock_pdf():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", "3000"))
+
     app.run(host="0.0.0.0", port=port)
